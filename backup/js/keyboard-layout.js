e282d31ea2ab1fa0d16187fa235fece8430a7cf6
KenzoRionel <jhonnyrionaldo96@gmail.com>
2025-08-26 03:38:15 +0700

    Menghapus file yang tidak perlu.

--- backup/js/keyboard-layout.js
+++
@@ -1,8 +0,0 @@
-// keyboard-layout.js
-export const keyLayout = [
-    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
-    ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
-    ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'],
-    ['ShiftLeft', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ShiftRight'],
-    ['ControlLeft', 'MetaLeft', 'AltLeft', '', 'Space', '', 'AltRight', 'MetaRight', 'ContextMenu', 'ControlRight']
-];
